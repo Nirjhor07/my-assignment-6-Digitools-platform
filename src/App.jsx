bee3d53148cbf4e8ec03a611e4e2d2436f6ad23a
Ashraf Hossain Akash <nirjhorakash07@gmail.com>
2026-03-31 13:31:55 +0600

    Added foooter but need to update this im supertired now

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import Stats from "./Components/Stats/Stats";
 import Cards from "./Components/cards/Cards";
 import { Suspense, useState } from "react";
 import { ToastContainer } from "react-toastify";
+import NewsLetter from "./Components/subcribe/NewsLetter";
+import Footer from "./Components/footer/Footer";
 
 //getting api data
 const cartPromise = async () => {
@@ -29,10 +31,15 @@ function App() {
         <Suspense
           fallback={<span className="loading loading-bars loading-xl"></span>}
         >
-          <Cards cart={cart} cardCount={cardCount} setCardCount={setCardCount}></Cards>
+          <Cards
+            cart={cart}
+            cardCount={cardCount}
+            setCardCount={setCardCount}
+          ></Cards>
         </Suspense>
       </main>
-
+      <NewsLetter></NewsLetter>
+      <Footer></Footer>
       <ToastContainer></ToastContainer>
     </>
   );
